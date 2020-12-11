6314c3890944c1e11f2c8aff5abcada1884485c2
Lê Nguyên Bình <75026941+nguyenbinh0702@users.noreply.github.com>
2020-12-11 16:23:21 +0700

    nop bai tap bien và kieu du lieu

---
+++ module1/7. Bien kieu du lieu và toan tu/Thuc hanh/su dung toan tu/Area.js
@@ -0,0 +1,8 @@
+var inputwidth;
+var inputheight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+document.write("The area is: " + area);
